5008a85b48ce704f26c32b1cd03b82cdd47bfb18
María González <mariagm173@hotmail.com>
2020-02-11 02:19:02 +0100

    spring-boot-backend-apirest 003 - Tablas usuarios y mails

---
+++ Unit 6 - Spring/spring-boot-backend-apirest/inUse/src/main/java/com/maria/springboot/backend/apirest/models/entity/Usuario.java
@@ -0,0 +1,76 @@
+package com.maria.springboot.backend.apirest.models.entity;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	
+	private String password;
+	
+	@OneToOne
+	@JoinColumn(name="idcliente",unique=true,nullable=false)
+	private Cliente cliente;
+	
+	@OneToMany(
+        mappedBy = "usuario",
+        cascade = CascadeType.ALL,
+        orphanRemoval = true
+	)
+	@JsonBackReference
+	private Set<Mail> mails = new HashSet<>();
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	
+	private static final long serialVersionUID = 5L;
+}
