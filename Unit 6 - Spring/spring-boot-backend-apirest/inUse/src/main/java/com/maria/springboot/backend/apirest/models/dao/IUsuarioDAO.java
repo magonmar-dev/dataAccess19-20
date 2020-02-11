cf7cd7ca0fc39f19c50a07c2ceb4d626fc8b961d
María González <mariagm173@hotmail.com>
2020-02-11 02:11:15 +0100

    spring-boot-backend-apirest 002 - Formularios Thymeleaf clientes y productos

--- Unit 6 - Spring/spring-boot-backend-apirest/inUse/src/main/java/com/maria/springboot/backend/apirest/models/dao/IUsuarioDAO.java
+++
@@ -1,9 +0,0 @@
-package com.maria.springboot.backend.apirest.models.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.maria.springboot.backend.apirest.models.entity.Usuario;
-
-public interface IUsuarioDAO extends CrudRepository<Usuario, Long> {
-
-}
